4a62ba7c240ec66eadf83d93807511b9f058d645
Vennela S Shetty <vennelashetty263@gmail.com>
2026-06-23 23:24:12 +0530

    Connection error resolved

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+// export const API = axios.create({
+//   baseURL: `${BASE_URL}/api`,
+// });
+
+const BASE_URL = "https://event-engine-backend.onrender.com";
 
 export const API = axios.create({
   baseURL: `${BASE_URL}/api`,
